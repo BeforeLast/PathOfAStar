c20fa9cb093617d5408dc1b3583a3f36980b108d
BeforeLast <Bfr.Last@Gmail.com>
2021-04-06 00:18:09 +0700

    split file for Node class

---
+++ src/node.py
@@ -0,0 +1,18 @@
+from location import *
+
+class Node :
+    def __init__(self, name, location) :
+        self.name = name
+        self.location = location
+        self.adjacentNode = []
+
+    # add adjacent node name
+    # input :
+    #   string : nodeName
+    # I.S. : self.adjacentNode == [..]
+    # F.S. : self.adjacentNode == [..,nodeName]
+    def addAdjNode(self,nodeName):
+        self.adjacentNode.append(nodeName)
+
+    def getDistanceToLocation(self, location):
+        self.location.euclidianDist(location)
