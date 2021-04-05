1cd175a998fad041f8b683d24c87c5020271b571
BeforeLast <Bfr.Last@Gmail.com>
2021-04-06 00:28:10 +0700

    Edit main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,4 @@ from graph import *
 G = Graph()
 G.fillGraphWithFile("../test/test1.txt")
 
-for node in G.nodes :
-    print(node.name + " " + str(node.point.x) + " " + str(node.point.y))
-for edge in G.edges :
-    print(edge.startNode.name + " " + edge.endNode.name + " " + str(edge.dist))
+G.AStar('D','A')
