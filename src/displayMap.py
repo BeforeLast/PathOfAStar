4f690be54abfe79cc1cf4e595c53dc6f629ceae6
billyjuliux <billyjulius01@gmail.com>
2021-04-06 12:36:47 +0700

    now pass 3 vars to js

--- src/displayMap.py
+++ src/displayMap.py
@@ -8,10 +8,9 @@ app.config['SECRET_KEY'] = 'thisisjustsomesecretkey'
 def displayMap() :
     # this should be the result of the pathing, to pass to js
     points = [[3.5898427621175326, 98.6483296391956], [3.5988800872318683, 98.65249242731477], [3.60012217704572, 98.64172067661467], [3.6062845004961233, 98.65024132694683]]
-    startNodeName = "Rumah Bije"
-    endNodeName = "Rumah doi"
+    paths = ["Rumah Bije", "SinggahA", "SinggahB", "Rumah Doi"]
     distance = 6969.69
-    return render_template("map.html", points = points, start = startNodeName, end = endNodeName, dist = distance)
+    return render_template("map.html", points = points, paths = paths, dist = distance)
 
 # run the flask
 if __name__ == '__main__':
