cab906901c0a4353aa8ffee55a45d406435f3cc1
BeforeLast <Bfr.Last@Gmail.com>
2021-04-06 23:15:58 +0700

    Fix errors

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from graph import *
 from flask import Flask , render_template , request
 # import matplotlib.pyplot as plt
 # import networkx as nx
+HTML_TEMPLATE = "map.html"
+
+def printDefaultTemplate(graphInfo=[]):
+    return render_template(HTML_TEMPLATE, route = "", coords = [], dist = 0 , graphInfo = graphInfo)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisjustsomesecretkey'
@@ -10,30 +14,36 @@ G = Graph()
 gInfo = []
 @app.route('/', methods=['GET', 'POST'])
 def main() :
-    return render_template("map.html", names = [], coords = [], dist = 0 , graphInfo = [])
+    return printDefaultTemplate()
 
 @app.route('/load-file/', methods=['GET', 'POST'])
 def loadFile() :
     fileNamePath = request.form['test']
-    print(fileNamePath)
+    if (len(fileNamePath)==0):
+        return printDefaultTemplate()
+    print("SELECTED FILE =",fileNamePath)
     G.fillGraphWithFile(fileNamePath)
     gInfo = G.toArray()
-    # print(G.toArray())
-    return render_template("map.html", names = [], coords = [], dist = 0 , graphInfo = gInfo)
+    return printDefaultTemplate(gInfo)
 
 @app.route('/calculate-route/', methods=['GET', 'POST'])
-def calculateRoute(startNode,goalNode):
+def calculateRoute():
+    startNode = request.form.get("startNodeName")
+    goalNode = request.form.get("goalNodeName")
+    if startNode==None or goalNode==None:
+        return printDefaultTemplate()
     pathNames, pathDist, pathSuccess = G.AStar(startNode,goalNode)
+    gInfo = G.toArray()
     if (pathSuccess) :
         pathCoords = []
         for i in range(len(pathNames)) :
-            currLoc = []
-            currLoc.append(G.getNodeLoc(pathNames[i]).x)
-            currLoc.append(G.getNodeLoc(pathNames[i]).y)
+            currLoc = [G.getNodeLoc(pathNames[i]).x, G.getNodeLoc(pathNames[i]).y]
             pathCoords.append(currLoc)
-        return render_template("map.html", coords = pathCoords, dist = pathDist , graphInfo = gInfo)
+        routeResult = '-'.join(pathNames)
+        return render_template(HTML_TEMPLATE, route = routeResult, coords = pathCoords, dist = pathDist , graphInfo = gInfo)
     else:
-        return render_template("map.html", coords = [], dist = 0 , graphInfo = gInfo)
+        badRouteResult = "No path found from " + startNode + " to " + goalNode
+        return render_template(HTML_TEMPLATE, route = badRouteResult, coords = [], dist = 0 , graphInfo = gInfo)
 
 if __name__ == '__main__':
     app.run(debug=True)
